9d17bd5ea15ce812f88c7398049aed854e062230
MUHAIMINUL ISLAM <raju.ridmik@gmail.com>
2024-10-12 09:28:59 +0600

    updated 2.3.1

--- fixtures/init.js
+++ fixtures/init.js
@@ -1,7 +1,7 @@
 const  { test :base } = require( '@playwright/test' );
 const { LogInPage } = require('../pages/LogInPage.spec.js');
 
-const {ProductPage} = require('../pages/ProductPage.spec.js');
+const { ProductPage } = require('../pages/ProductPage.spec.js');
 
 export const test = base.extend({
     loginCredObj : async ({ page}, use) =>{
