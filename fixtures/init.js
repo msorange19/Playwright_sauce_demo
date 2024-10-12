bd38858ae36703e36d32aa7391e92d4d99b4c817
MUHAIMINUL ISLAM <raju.ridmik@gmail.com>
2024-10-12 09:28:24 +0600

    updated 2.3

--- fixtures/init.js
+++ fixtures/init.js
@@ -1,5 +1,6 @@
 const  { test :base } = require( '@playwright/test' );
 const { LogInPage } = require('../pages/LogInPage.spec.js');
+
 const {ProductPage} = require('../pages/ProductPage.spec.js');
 
 export const test = base.extend({
